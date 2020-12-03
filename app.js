55fea0c8a007bd70fcf55722bfc936e58d584483
Alex <Koblosh.Alex@gmail.com>
2020-12-03 11:27:49 -0500

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const server = app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+app.get("/", (req, res, next) => {
+  const payload = {
+    pageTitle: "Home",
+  };
+  res.status(200).render("home", payload);
+});
